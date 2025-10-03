1a3803d270e8d9d0316dc9f27ebba92f374d8c7c
JVTemponi <jvictortemponi@gmail.com>
2025-10-03 01:36:06 -0300

    leitura token

--- code/src/config/config.py
+++ code/src/config/config.py
@@ -1,15 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# Carrega as variáveis do arquivo .env para o ambiente
 load_dotenv()
 
-# Obtém o token do GitHub a partir das variáveis de ambiente
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-# Validação para garantir que o token foi definido
 if not GITHUB_TOKEN:
     raise ValueError(
         "A variável de ambiente GITHUB_TOKEN não foi definida. "
         "Crie um arquivo .env na raiz do projeto e adicione a variável."
     )
+
